c6ffbfeef3bf7bbffbdc77df48ef0a487b1cdb94
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-06-22 10:00:37 +0530

    App.jsx update with routes

--- Navbar/fixed-navbar-app/src/App.jsx
+++ Navbar/fixed-navbar-app/src/App.jsx
@@ -1,10 +1,27 @@
 import Navbar from "./Navbar.jsx";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Members from './pages/Members.jsx'
+import Join from "./pages/Join.jsx";
+import Contact from "./pages/Contact.jsx";
+import Events from "./pages/Events.jsx";
+import Home from "./pages/Home.jsx";
+
 
 function App() {
 
     return(
         <div>
-            <Navbar></Navbar>
+
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/top-board" element={<Members />} />
+                    <Route path="/join" element={<Join />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/events" element={<Events />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
