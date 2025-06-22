4b1f29e8d5b0e1ce093608ff5a07b7d5db0b86e4
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-06-22 09:59:16 +0530

    Creating Event,Join,Members

---
+++ Navbar/fixed-navbar-app/src/pages/Events.jsx
@@ -0,0 +1,11 @@
+
+
+function Events(){
+    return(
+        <>
+            <h1>Events Page</h1>
+        </>
+    );
+}
+
+export default Events
---
+++ Navbar/fixed-navbar-app/src/pages/Members.jsx
@@ -0,0 +1,11 @@
+
+
+function Members(){
+    return(
+        <>
+            <h1>Members Page</h1>
+        </>
+    );
+}
+
+export default Members
