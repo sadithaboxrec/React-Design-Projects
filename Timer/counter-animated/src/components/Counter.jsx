41332962994371fd578cdd185d256fc8314b7424
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-06-25 13:17:29 +0530

    Adding key to id

--- Timer/counter-animated/src/components/Counter.jsx
+++ Timer/counter-animated/src/components/Counter.jsx
@@ -40,7 +40,7 @@ function Counter() {
             <div className='hero counter'>
                 <div className='container grid3 grid4'>
                     {project.map((item) => (
-                        <div className='box' data-aos='zoom-in'>
+                        <div className='box' data-aos='zoom-in' key={item.id}>
                             <i>{item.icon}</i>
                             <h1 className='heading'>
                                 <CountUp enableScrollSpy duration={2} end={item.num} />
