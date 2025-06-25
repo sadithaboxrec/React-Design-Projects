054949c731ea8339c72e99b469816b5c3ea2d8d7
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-06-25 19:52:48 +0530

    Mobile Responsiveness

--- Navbar/fixed-navbar-app/src/Navbar.jsx
+++ Navbar/fixed-navbar-app/src/Navbar.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 import './Navbar.css'
 import { Link } from "react-router-dom";
 
 function Navbar (){
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleToggle = () => {
+        setMenuOpen(!menuOpen);
+    };
 
             return (
                     <nav className="navbar">
                         <div className="navbar-container">
                             {/*<a href="index.html" className="navbar-logo">Saditha</a>*/}
                             <Link to="/" className="navbar-logo">Saditha</Link>
-                            <button className="navbar-toggle">
+                            <button className={`navbar-toggle ${menuOpen ? "active" : ""}`}
+                                    onClick={handleToggle}>
                                 <span className="bar"></span>
                                 <span className="bar"></span>
                                 <span className="bar"></span>
                             </button>
-                            <ul className="navbar-menu">
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="/top-board">Top Board</Link></li>
-                                <li><Link to="/events">Events</Link></li>
-                                <li><Link to="/join">Join Us</Link></li>
-                                <li><Link to="/contact">Contact Us</Link></li>
+                            <ul className={`navbar-menu ${menuOpen ? "active" : ""}`}>
+                                <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+                                <li><Link to="/top-board" onClick={() => setMenuOpen(false)}>Top Board</Link></li>
+                                <li><Link to="/events" onClick={() => setMenuOpen(false)}>Events</Link></li>
+                                <li><Link to="/join" onClick={() => setMenuOpen(false)}>Join Us</Link></li>
+                                <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact Us</Link></li>
 
                             </ul>
                         </div>
