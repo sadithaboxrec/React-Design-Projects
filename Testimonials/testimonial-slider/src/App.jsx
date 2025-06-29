d0bcd1c366efa9c82797467bd08e0d70ae974f4c
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-06-29 15:21:04 +0530

    app.jsx

---
+++ Testimonials/testimonial-slider/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+import Testimonials from "./Testimonials/Testimonials.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Testimonials/>
+    </>
+  )
+}
+
+export default App
