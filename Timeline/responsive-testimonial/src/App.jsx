a698a17357375d1091667eedee405fdf928bba61
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-07-05 17:26:42 +0530

    initial commit

---
+++ Timeline/responsive-testimonial/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Testimonials from "./Testimonials/Testimonials.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Testimonials/>
+    </>
+  )
+}
+
+export default App
