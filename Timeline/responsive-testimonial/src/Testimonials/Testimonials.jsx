6287e927a27f72a30008327b8f49ab4f4cd7a556
Saditha Fernando <sadithafernandods2002@gmail.com>
2025-07-05 17:27:06 +0530

    creating basic format

---
+++ Timeline/responsive-testimonial/src/Testimonials/Testimonials.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import './styles.css'
+import { timelineData } from './data';
+
+
+
+function MyTimeline() {
+    return (
+        <div className="page-wrapper">
+            <div className="section-timeline">
+                <div className="timeline_component">
+                    <div className="timeline_progress">
+                        <div className="timeline_progress-bar"></div>
+                    </div>
+
+                    {timelineData.map((item) => (
+                        <div className="timeline_item" id={item.id} key={item.id}>
+                            <div className="timeline_left">
+                                <div className="timeline_date-text">{item.date}</div>
+                            </div>
+                            <div className="timeline_centre">
+                                <div className="timeline_circle"></div>
+                            </div>
+                            <div className="timeline_right">
+                                <div className="margin-bottom-xlarge">
+                                    <div className="timeline_text">{item.text}</div>
+
+                                    <div className="social-icons">
+                                        <a href={item.socialLinks.facebook} aria-label="Facebook">
+                                            <i className="bi bi-facebook"></i>
+                                        </a>
+                                        <a href={item.socialLinks.twitter} aria-label="Twitter">
+                                            <i className="bi bi-twitter-x"></i>
+                                        </a>
+                                        <a href={item.socialLinks.instagram} aria-label="Instagram">
+                                            <i className="bi bi-instagram"></i>
+                                        </a>
+                                        <a href={item.socialLinks.linkedin} aria-label="LinkedIn">
+                                            <i className="bi bi-linkedin"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="timeline_image-wrapper">
+                                    <img src={item.image} alt={item.alt} loading="lazy" width="480" />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                    <div className="overlay-fade-top"></div>
+                    <div className="overlay-fade-bottom"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MyTimeline;
---
+++ Timeline/responsive-testimonial/src/Testimonials/data.js
@@ -0,0 +1,77 @@
+import blackImage from '../assets/black.jpg';
+
+
+
+
+export const timelineData = [
+    {
+        id: 1,
+        date: 'January 2010',
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           Nobis quibusdam perspiciatis sit sapiente ipsum quod quos.`,
+        image: blackImage,
+        imageAlt: 'Tesla Roadster',
+        socialLinks: {
+            facebook: '#',
+            twitter: '#',
+            instagram: '#',
+            linkedin: '#',
+        },
+    },
+    {
+        id: 2,
+        date: 'March 2015',
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           Nobis quibusdam perspiciatis sit sapiente ipsum quod quos.`,
+        image: blackImage,
+        imageAlt: 'Chevy Bolt EV',
+        socialLinks: {
+            facebook: '#',
+            twitter: '#',
+            instagram: '#',
+            linkedin: '#',
+        },
+    },
+    {
+        id: 3,
+        date: 'July 2020',
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           Nobis quibusdam perspiciatis sit sapiente ipsum quod quos.`,
+        image: blackImage,
+        imageAlt: 'Government EV Announcement',
+        socialLinks: {
+            facebook: '#',
+            twitter: '#',
+            instagram: '#',
+            linkedin: '#',
+        },
+    },
+    {
+        id: 4,
+        date: 'February 2021',
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           Nobis quibusdam perspiciatis sit sapiente ipsum quod quos.`,
+        image: blackImage,
+        imageAlt: 'Tesla Milestone',
+        socialLinks: {
+            facebook: '#',
+            twitter: '#',
+            instagram: '#',
+            linkedin: '#',
+        },
+    },
+    {
+        id: 5,
+        date: 'October 2022',
+        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+           Nobis quibusdam perspiciatis sit sapiente ipsum quod quos.`,
+        image: blackImage,
+        imageAlt: 'EV Sales Milestone',
+        socialLinks: {
+            facebook: '#',
+            twitter: '#',
+            instagram: '#',
+            linkedin: '#',
+        },
+    },
+];
